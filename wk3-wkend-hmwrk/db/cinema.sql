47b2b2dc6bfd7e660db48237ff505de58f1a5530
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-16 17:06:36 +0000

    Database and SqlRunner set up

---
+++ wk3-wkend-hmwrk/db/cinema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS screenings;
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT2
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+);
+
+CREATE TABLE screenings (
+  id SERIAL4 PRIMARY KEY,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  time VARCHAR(255),
+  tickets_available INT2
+);
